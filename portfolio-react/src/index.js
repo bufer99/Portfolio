8132c8ae5bb25ead0ec0b3537d3a9ba74f8b9e16
bufer99 <bufer99@gmail.com>
2022-08-13 16:11:25 +0200

    Update index.js

--- portfolio-react/src/index.js
+++ portfolio-react/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './state/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter basename="https://bufer99.com">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </Provider>
