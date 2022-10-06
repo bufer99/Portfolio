4627a858af098953f56f379e3c626edcb714290e
bufer99 <bufer99@gmail.com>
2022-10-06 16:29:26 +0200

    fix grammar

--- portfolio-react/src/state/language.js
+++ portfolio-react/src/state/language.js
@@ -10,7 +10,7 @@ export const languages = {
             'intro-first':'Szia,',
             'intro-second':"A nevem",
             'intro-third': 'Langó András.',
-            'text':`Langó András vagyok, 23 éves, 7. féléves <abbr title="programtervező informatikus">proginf.</abbr> BSc hallagató az <span class="img_ref" id="education">ELTE-IK</span>-n. <span class="img_ref" id="city">Budaörsön</span> élek. 2021-ben döntöttem el hogy webfejlesztéssel akarok foglalkozni. Hosszú távon fullstack programozóként képzelném el magam. Szeretek sportolni, jelenleg egy térdsérülésből épülökf fel, ezért csak az edzőteremre korlátozódtam egy darabig. 2021-ben elvégeztem az <abbr title="Magyar Labdarúgó-szövetség">MLSZ</abbr> labdarúgó <span class="img_ref" id="referee">játékvezetői</span> képzést. Szabadidőmben programozok, próbálom bővíteni a tudásom.`
+            'text':`Langó András vagyok, 23 éves, 7. féléves <abbr title="programtervező informatikus">proginf.</abbr> BSc hallagató az <span class="img_ref" id="education">ELTE-IK</span>-n. <span class="img_ref" id="city">Budaörsön</span> élek. 2021-ben döntöttem el hogy webfejlesztéssel akarok foglalkozni. Hosszú távon fullstack programozóként képzelném el magam. Szeretek sportolni. 2021-ben elvégeztem az <abbr title="Magyar Labdarúgó-szövetség">MLSZ</abbr> labdarúgó <span class="img_ref" id="referee">játékvezetői</span> képzést. Szabadidőmben programozok, edzőterembe járok és próbálom bővíteni a tudásom.`
         },
         hobbies:[
             'Konditerem',
@@ -32,7 +32,7 @@ export const languages = {
             'intro-first':'Hi,',
             'intro-second':"I'm",
             'intro-third': 'Andras Lango.',
-            'text':`This is my 7th semester at <span class="img_ref" id="education"> ELTE Faculty of Informatics </span> as computer science student. I live in <span class="img_ref" id="city"> Budaörs </span>. In 2021 I made up my mind that I want to work as a web developer and become a full stack developer over the long term. I like all kinds of sport especially football. Currently I'm recovering from an ACL surgery, but latey I can go to the local gym at least. In 2021 I attended a <span class="img_ref" id="referee"> football referee</span> training organized by the Hungarian Football Federation. In my spare time I'm developing websites and improving my skills in that matter.`
+            'text':`This is my 7th semester at <span class="img_ref" id="education"> ELTE Faculty of Informatics </span> as computer science student. I live in <span class="img_ref" id="city"> Budaörs </span>. In 2021 I made up my mind that I want to work as a web developer and become a full stack developer over the long term. I like all kinds of sport especially football. In 2021 I attended a <span class="img_ref" id="referee"> football referee</span> training organized by the Hungarian Football Federation. In my spare time I'm developing websites, improving my programming skills and hitting the gym.`
         },
         hobbies:[
             'Going to Gym',
